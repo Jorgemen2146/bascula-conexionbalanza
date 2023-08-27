908e1b23746c5960ffc15d6b4f091dddf82e6a5e
Jorge Luis <gonzales823@hotmail.com>
2023-08-26 20:54:42 -0500

    Add files via upload

---
+++ bascula.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace bascula
+{
+    partial class bascula
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.spBalanza = new System.IO.Ports.SerialPort(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblestado = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblultimahora = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblultimopesaje = new System.Windows.Forms.Label();
+            this.lblerror = new System.Windows.Forms.Label();
+            this.timerconexion = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 246);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Estado";
+            // 
+            // lblestado
+            // 
+            this.lblestado.AutoSize = true;
+            this.lblestado.Location = new System.Drawing.Point(212, 42);
+            this.lblestado.Name = "lblestado";
+            this.lblestado.Size = new System.Drawing.Size(63, 16);
+            this.lblestado.TabIndex = 2;
+            this.lblestado.Text = "lblestado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ultima Hora Pesaje";
+            // 
+            // lblultimahora
+            // 
+            this.lblultimahora.AutoSize = true;
+            this.lblultimahora.Location = new System.Drawing.Point(215, 79);
+            this.lblultimahora.Name = "lblultimahora";
+            this.lblultimahora.Size = new System.Drawing.Size(0, 16);
+            this.lblultimahora.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(45, 128);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Ultimo Pesaje";
+            // 
+            // lblultimopesaje
+            // 
+            this.lblultimopesaje.AutoSize = true;
+            this.lblultimopesaje.Location = new System.Drawing.Point(215, 127);
+            this.lblultimopesaje.Name = "lblultimopesaje";
+            this.lblultimopesaje.Size = new System.Drawing.Size(0, 16);
+            this.lblultimopesaje.TabIndex = 6;
+            // 
+            // lblerror
+            // 
+            this.lblerror.AutoSize = true;
+            this.lblerror.Location = new System.Drawing.Point(57, 273);
+            this.lblerror.Name = "lblerror";
+            this.lblerror.Size = new System.Drawing.Size(0, 16);
+            this.lblerror.TabIndex = 7;
+            // 
+            // timerconexion
+            // 
+            this.timerconexion.Enabled = true;
+            this.timerconexion.Interval = 30000;
+            this.timerconexion.Tick += new System.EventHandler(this.timerconexion_Tick);
+            // 
+            // bascula
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 344);
+            this.Controls.Add(this.lblerror);
+            this.Controls.Add(this.lblultimopesaje);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblultimahora);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblestado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "bascula";
+            this.Text = "bascula";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.bascula_FormClosing);
+            this.Load += new System.EventHandler(this.bascula_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort spBalanza;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblestado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblultimahora;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblultimopesaje;
+        private System.Windows.Forms.Label lblerror;
+        private System.Windows.Forms.Timer timerconexion;
+    }
+}
+
---
+++ bascula.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Configuration;
+using System.Runtime.Remoting.Contexts;
+
+namespace bascula
+{
+    public partial class bascula : Form
+    {   
+        int posicion = Convert.ToInt32((ConfigurationManager.AppSettings["posicion"].ToString()));
+        string dividir = ConfigurationManager.AppSettings["didivircadena"];
+
+        string ultimopesaje;
+        string ultimahorapesaje;
+        public bascula()
+        {
+            InitializeComponent();
+        }
+        
+        private void bascula_Load(object sender, EventArgs e)
+        {
+            spBalanza.DataReceived += new SerialDataReceivedEventHandler(spBalanza_DataReceived);
+            Conectarbalanza();
+
+        }
+
+        private void spBalanza_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            System.Threading.Thread.Sleep(5000);
+            try
+            {
+
+                var cadenaBalanza = spBalanza.ReadExisting().Trim();
+
+                Guardarhistorialcadenacompleta(cadenaBalanza);
+
+                string[] arreglo = cadenaBalanza.Split(' ');
+
+                
+
+                //StreamWriter sw = new StreamWriter("D:\\pesaje.txt");
+                //StreamReader sr = new StreamReader("D:\\pesaje.txt");
+                //Read the first line of text
+                //var line = sr.ReadLine();
+
+                //sw.WriteLine(cadenaenviar);
+                if (arreglo.Count()>5)
+                {
+                    string cadenaenviar = devolverpesado(arreglo);
+
+                    if (cadenaenviar != "ERROR")
+                    {
+                        GuardarPesaje(cadenaenviar);
+                        Historialdepesajes(cadenaenviar);
+                        ultimopesaje = cadenaenviar;
+                        this.Invoke(new EventHandler(Actualizar));
+                    }
+                }
+                
+                    
+                    
+                    
+                //cargarcontroles(cadenaenviar);
+
+
+
+                //sw.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Guardarlogerrores(ex.Message + "   " + ex.StackTrace.ToString());
+            }
+        }
+
+        public void Actualizar(object sender, EventArgs e)
+        {
+            lblultimopesaje.Text = ultimopesaje;
+
+          
+            lblultimahora.Text = DateTime.Now.ToString("F");
+        }
+         
+
+        public void Historialdepesajes(string cadena)
+        {
+            var rutaarchivo = ConfigurationManager.AppSettings["RutaPesajetxt"];
+            try
+            {
+                if (!System.IO.File.Exists(rutaarchivo + @"HISTORIALPESO.txt"))
+                {
+                    var myFile = System.IO.File.Create(rutaarchivo + @"HISTORIALPESO.txt");
+
+                    myFile.Close();
+                }
+
+                System.IO.File.AppendAllText(rutaarchivo + @"HISTORIALPESO.txt", cadena + "\n\n\n");
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        public string devolverpesado(string[] lista)
+        {
+            try { 
+            List<string> pesosencontrados= new List<string>();
+            int pesajedevolver = 0;
+
+            for (int i = 0; i < lista.Count(); i++)
+            {
+                if (lista[i].ToString().StartsWith("lb"))
+                {
+                    pesosencontrados.Add( lista[i-1].ToString());
+                }
+            }
+            //verificamos si es pesaje o repesaje
+
+            if (pesosencontrados.Count()==1)
+            {
+                //es pesaje
+                return pesosencontrados[0];
+
+            }
+            else
+            {
+                //es repesaje
+                return pesosencontrados[1];
+
+            }
+            }catch (Exception ex)
+            {
+                Guardarlogerrores(ex.Message + "   " + ex.StackTrace.ToString());
+                return "ERROR";
+            }
+
+
+
+        }
+
+        public void GuardarPesaje(string cadena)
+        {
+            var rutaarchivo = ConfigurationManager.AppSettings["RutaPesajetxt"];
+            try
+            {
+                if (!System.IO.File.Exists(rutaarchivo + @"PESO.txt"))
+                {
+                    var myFile = System.IO.File.Create(rutaarchivo + @"PESO.txt");
+
+                    myFile.Close();
+                }
+
+               
+               
+                    System.IO.File.WriteAllText(rutaarchivo + @"PESO.txt", cadena);
+
+
+     
+
+                //System.IO.File.AppendAllText(rutaarchivo + @"CADENACOMPLETA.txt", cadena+ "\n\n\n");
+                //label1.Text = cadena;
+
+                //MessageBox.Show(cadena);
+
+
+            }
+            catch (Exception ex)
+            {
+                Guardarlogerrores(ex.Message +"   " + ex.StackTrace.ToString());
+            }
+        }
+
+        public void Guardarhistorialcadenacompleta(string cadena)
+        {
+            var rutaarchivo = ConfigurationManager.AppSettings["RutaPesajetxt"];
+            try
+            {
+                if (!System.IO.File.Exists(rutaarchivo + @"CADENACOMPLETA.txt"))
+                {
+                    var myFile = System.IO.File.Create(rutaarchivo + @"CADENACOMPLETA.txt");
+
+                    myFile.Close();
+                }
+
+
+                System.IO.File.AppendAllText(rutaarchivo + @"CADENACOMPLETA.txt", cadena + "\n--------------------------------------------------------\n\n");
+                System.IO.File.AppendAllText(rutaarchivo + @"CADENACOMPLETA.txt", "\n");
+
+
+
+
+                //System.IO.File.AppendAllText(rutaarchivo + @"CADENACOMPLETA.txt", cadena+ "\n\n\n");
+                //label1.Text = cadena;
+
+                //MessageBox.Show(cadena);
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public void Guardarlogerrores(string error)
+        {
+            var rutaarchivo = ConfigurationManager.AppSettings["RutaPesajetxt"];
+            try
+            {
+                if (!System.IO.File.Exists(rutaarchivo + @"LogErrores.txt"))
+                {
+                    var myFile = System.IO.File.Create(rutaarchivo + @"LogErrores.txt");
+
+                    myFile.Close();
+                }
+
+
+
+                System.IO.File.AppendAllText(rutaarchivo + @"LogErrores.txt", DateTime.Now.ToString("F")+ "   " + error + "\n"); ;
+
+
+
+
+                //System.IO.File.AppendAllText(rutaarchivo + @"CADENACOMPLETA.txt", cadena+ "\n\n\n");
+                //label1.Text = cadena;
+
+                //MessageBox.Show(cadena);
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+
+
+        public void Conectarbalanza()
+        {
+            //spBalanza.DataReceived += new SerialDataReceivedEventHandler(spBalanza_DataReceived);
+            try
+            {
+                CerrarPuerto();
+
+                //configuracion de la balanza
+
+                var Paridad =  ConfigurationManager.AppSettings["Paridad"]; 
+                var Puerto = ConfigurationManager.AppSettings["Puerto"];
+                var Parada = ConfigurationManager.AppSettings["Parada"];
+                var CtrlFlujo = "";
+                var Plantilla = "";
+                var Bitdatos = ConfigurationManager.AppSettings["Bitdatos"];
+                var Baud = ConfigurationManager.AppSettings["Baud"];
+
+                spBalanza.PortName = Puerto;
+
+                switch (Paridad.ToUpper())
+                {
+                    case "NONE":
+                        spBalanza.Parity = System.IO.Ports.Parity.None;
+                        break;
+                    case "PAR":
+                        spBalanza.Parity = System.IO.Ports.Parity.Even;
+                        break;
+                    case "IMPAR":
+                        spBalanza.Parity = System.IO.Ports.Parity.Odd;
+                        break;
+                    case "ESPACIO":
+                        spBalanza.Parity = System.IO.Ports.Parity.Space;
+                        break;
+                    case "MARCA":
+                        spBalanza.Parity = System.IO.Ports.Parity.Mark;
+                        break;
+                }
+                switch (Parada.ToUpper())
+                {
+                    case "UNO":
+                        spBalanza.StopBits = System.IO.Ports.StopBits.One;
+                        break;
+                    case "UNOYMEDIO":
+                        spBalanza.StopBits = System.IO.Ports.StopBits.OnePointFive;
+                        break;
+                    case "DOS":
+                        spBalanza.StopBits = System.IO.Ports.StopBits.Two;
+                        break;
+                }
+                switch (CtrlFlujo.ToUpper())
+                {
+                    case "NONE":
+                        spBalanza.Handshake = System.IO.Ports.Handshake.None;
+                        break;
+                    case "HARDWARE":
+                        spBalanza.Handshake = System.IO.Ports.Handshake.RequestToSend;
+                        break;
+                    case "HARDWARE_XONXOFF":
+                        spBalanza.Handshake = System.IO.Ports.Handshake.RequestToSendXOnXOff;
+                        break;
+                    case "XONXOFF":
+                        spBalanza.Handshake = System.IO.Ports.Handshake.XOnXOff;
+                        break;
+                }
+                spBalanza.DataBits = Convert.ToInt32(Bitdatos);
+                spBalanza.BaudRate = Convert.ToInt32(Baud);
+                string estado = "";
+                //validando el puerto
+                //var isValid = SerialPort.GetPortNames().Any(x => string.Compare(x, spBalanza.PortName, true) == 0);
+                //if (!isValid)
+                //{
+
+                //    estado = "Invalido";
+
+                //}
+                //else
+                //{
+                //    estado = "Valido";
+                //}
+
+                if (!spBalanza.IsOpen)
+
+                    spBalanza.Open();
+
+
+                label1.Text = "Se realizo la conexion con exito";
+                lblestado.Text = "CONECTADO";
+                lblestado.BackColor = Color.Green;
+
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Hubo un problema para conectar   " + ex.Message;
+                Guardarlogerrores(ex.Message + "   " + ex.StackTrace.ToString());
+            }
+
+
+        }
+
+        private void CerrarPuerto()
+        {
+
+            try
+            {
+                if (spBalanza.IsOpen)
+                {
+                    spBalanza.Close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void bascula_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CerrarPuerto();
+        }
+
+        public void verificarconexion(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!spBalanza.IsOpen)
+                {
+                    //abrimos la conexion denuevo
+                    Conectarbalanza();
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                Guardarlogerrores(ex.Message + "   " + ex.StackTrace.ToString());
+                label1.Text = "Hubo un problema para conectar   " + ex.Message;
+            }
+        }
+
+        private void timerconexion_Tick(object sender, EventArgs e)
+        {
+            this.Invoke(new EventHandler(verificarconexion));
+            
+        }
+    }
+}
